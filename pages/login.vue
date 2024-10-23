4c6c8fbd320a2cf3d66c3757ad5e7073fa63b8f0
dvtinh99 <tinh.doan@codecomplete.jp>
2024-10-23 15:08:12 +0700

    first cm

---
+++ pages/login.vue
@@ -0,0 +1,14 @@
+<template>
+    <SectionsFormLogin/>
+</template>
+<script setup lang="ts">
+useHead({
+  title: 'Podux - Home',
+  meta: [
+    { name: 'description', content: 'Podux Podcast landing page website.' },
+  ],
+  link: [
+    { rel: 'icon', type: 'image/png', href: '/logo.png' }
+  ],
+})
+</script>
